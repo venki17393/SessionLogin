4c7d9d7ada549b53b11e2b735ae21901987ba1f4
venki17393 <venki17393@gmail.com>
2017-05-16 15:59:18 +0530

    Taken care of null pointer expetion if session is not active

--- src/com/session/SessionHelper.java
+++ src/com/session/SessionHelper.java
@@ -6,9 +6,13 @@ import javax.servlet.http.HttpSession;
 public class SessionHelper {
 	public static String currentUser(HttpServletRequest req){
 		HttpSession session =req.getSession();
+		if(session!= null){
 		String s1 = (String) session.getAttribute("email");
 		return s1;
-		
+		}
+		else {
+			return null ;
+		}
 	}
 
 }
