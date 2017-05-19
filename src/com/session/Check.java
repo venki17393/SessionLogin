ac647150961fd22a65d10401bf815318ff4a4a8b
venki17393 <venki17393@gmail.com>
2017-05-19 13:26:08 +0530

    We have used more of a dynamic check at the login page and also for signup we are using get type of method

--- src/com/session/Check.java
+++ src/com/session/Check.java
@@ -1,7 +1,6 @@
 package com.session;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+
 public class Check extends HttpServlet {
+
+	DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
@@ -24,18 +31,29 @@ public class Check extends HttpServlet {
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		resp.setContentType("text/html");
-		PrintWriter o = resp.getWriter();
+		int count = 0;
 		String email = req.getParameter("email");
 		String password = req.getParameter("password");
-
-		if ((email.equals("admin@testing.com")) && (password.equals("123"))){
+		Query q1 = new Query("Contact");
+		PreparedQuery prepd = ds.prepare(q1);
+		for (Entity ent : prepd.asIterable()) {
+			String dname = (String) ent.getProperty("Email");
+			String dpass = ent.getProperty("Password").toString();
+			if ((dname.equals(email)) && (dpass.equals(password))) {
 				HttpSession s1 = req.getSession();
 				s1.setAttribute("email", email);
-				req.getRequestDispatcher("a.html").include(req, resp);
+				count = 1;
+				break;
+			} else {
+				count = 0;
+
 			}
-		 else {
+		}
+		if (count == 1) {
+			req.getRequestDispatcher("a.html").include(req, resp);
+		} else {
 			resp.sendRedirect("login");
+
 		}
 	}
-
 }
--- src/com/session/SignUp.java
+++ src/com/session/SignUp.java
@@ -1,7 +1,6 @@
 package com.session;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,11 +11,12 @@ import javax.servlet.http.HttpSession;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
 
 public class SignUp extends HttpServlet {
 	DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
@@ -26,11 +26,13 @@ public class SignUp extends HttpServlet {
 
 	}
 
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException  {
 		String name = req.getParameter("name");
 		String password = req.getParameter("password");
 		String email = req.getParameter("email");
-
+		
+		
+		// to check if email is present using iteration
 		// Query q1 = new Query("Contact");
 		// PreparedQuery prepare = ds.prepare(q1);
 		// boolean isPresent = false;
@@ -48,29 +50,50 @@ public class SignUp extends HttpServlet {
 		// if (isPresent) {
 		// }
 		
-		Query q = new Query("Contact").addFilter("Email", FilterOperator.EQUAL, email);
+		
+		// To check if the email is present using Query
+		/*Query q = new Query("Contact").addFilter("Email", FilterOperator.EQUAL, email);
 		PreparedQuery pd=ds.prepare(q);
-		int entities = pd.countEntities();
-		System.out.println(pd);
-		System.out.println(entities);
-		//boolean result = ds.
-		if (entities != 0) {
-
-			resp.sendRedirect("signup");
-		} else {
-			Entity contact = new Entity("Contact", email);
-			contact.setProperty("Name", name);
-			contact.setProperty("Password", password);
-			contact.setProperty("Email", email);
-			ds.put(contact);
-			HttpSession s1 = req.getSession(false);
-			String session = SessionHelper.currentUser(req);
-			if (session != null) {
-				s1.invalidate();
-			}
-			s1 = req.getSession();
-			s1.setAttribute("email", email);
-			resp.sendRedirect("dashboard");
+		int entities = pd.countEntities();*/
+		/*Key emaill = KeyFactory.createKey(email, "Conatct");
+		try {
+			Entity temp = ds.get(emaill);
+		} catch (EntityNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}*/
+		
+		Key emaill = KeyFactory.createKey("Contact",email);
+		try {
+			Entity temp = ds.get(emaill);
+			System.out.println(temp);
+				resp.sendRedirect("signup");
+			
 		}
-	}
+		 catch (EntityNotFoundException e1) {
+			// TODO Auto-generated catch block
+			 Entity contact = new Entity("Contact", email);
+				contact.setProperty("Name", name);
+				contact.setProperty("Password", password);
+				contact.setProperty("Email", email);
+				ds.put(contact);
+				HttpSession s1 = req.getSession(false);
+				String session = SessionHelper.currentUser(req);
+				if (session != null) {
+					s1.invalidate();
+				}
+				s1 = req.getSession();
+				s1.setAttribute("email", email);
+				resp.sendRedirect("dashboard");
+			
+			//e1.printStackTrace();
+		}
+	
+	//	System.out.println(temp.getKey());
+		//SSystem.out.println(temp.getProperty("Email"));
+		//System.out.println(pd);
+		//System.out.println(entities);
+		
+		
+}
 }
